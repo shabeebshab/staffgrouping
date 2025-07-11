cf3a7441effaa49129976be466930f5e794456f0
shabeebshab <shabeebshabi508@gmail.com>
2025-07-11 22:23:04 +0530

    Update: made changes to print page layout and styling

--- staffgrouperap/forms.py
+++ staffgrouperap/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class RawDataForm(forms.Form):
-    raw_data = forms.CharField(label='', widget=forms.Textarea(attrs={'rows': 10, 'placeholder': 'copying text must start with #id like 1,2,3..'}))
+    raw_data = forms.CharField(label='', widget=forms.Textarea(attrs={'rows': 10, 'placeholder': 'copying text must start with  1,2,3..'}))
